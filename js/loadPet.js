298a517bc5308870aa79a7311726122d6311a14e
sadhin28 <taosifsadhin113@gmail.com>
2025-04-22 02:12:29 +0600

    add details button

--- js/loadPet.js
+++ js/loadPet.js
@@ -15,9 +15,7 @@ const displayPet = (pet) => {
         console.log(element)
         const PetContainer = document.getElementById('petContainer');
         const card = document.createElement('div');
-        card.innerHTML =
-                          
-        `
+        card.innerHTML =`
                 <div class="card w-80 bg-base-100 shadow-sm mx-auto">
                      <div class="card-body">
                     <div>
@@ -28,7 +26,12 @@ const displayPet = (pet) => {
                         <p><i class="fa-solid fa-cake-candles"></i> Birth : ${element.date_of_birth}</p>
                         <p><i class="fa-solid fa-mercury"></i> Gender : ${element.gender}</p>
                         <p><i class="fa-solid fa-dollar-sign"></i> Price : ${element.price} $</p>
-
+                        <div class="divider"></div>
+                        <div class="flex justify-between" >
+                            <button class="btn rounded-xl border-cyan-200 font-bold text-[#0E7A81]"><i class=" border-cyan-950 text-black fa-solid fa-thumbs-up"></i></button>
+                            <button class="btn rounded-xl border-cyan-200 font-bold text-[#0E7A81]">Adopt</button>
+                            <button class="btn rounded-xl border-cyan-200 font-bold text-[#0E7A81]">Details</i></button>
+                        </div>
                     </div>
                  </div>
                  
