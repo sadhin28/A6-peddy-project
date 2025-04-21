9599a60455b5a1370d9307c833084ca2eace6f82
sadhin28 <taosifsadhin113@gmail.com>
2025-04-21 22:04:12 +0600

    add button dynamicly

--- js/categories.js
+++ js/categories.js
@@ -1,7 +1,24 @@
+//loadCatagories
 const loadCatagories = () => {
      fetch('https://openapi.programming-hero.com/api/peddy/categories')
      .then(res => res.json())
-     .then((data)=>console.log(data.categories))
+     .then((data)=>displayCatagories(data.categories))
      .catch((error) => console.log(error) )
  }
+
+//displaycatagories
+const displayCatagories = (data)=>{
+     const CategoriesContainer = document.getElementById('categoriesContainer');
+     data.forEach(element => {
+          console.log()
+         const buttonCatagories = document.createElement('div');
+         buttonCatagories.innerHTML=`
+       
+                <button  class= "btn lg:p-6 lg:text-2xl lg:2  " ><img class="h-5 w-5 lg:h-10 lg:w-10 md:h-8 md:w-8" src=${element.category_icon} />${element.category}</button>
+          
+
+         `
+         CategoriesContainer.append(buttonCatagories)
+     });
+}
 loadCatagories()
