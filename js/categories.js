7ec3bc5a233b4d7bc67cfcb5cb89b3f693f0d846
sadhin28 <taosifsadhin113@gmail.com>
2025-04-22 00:50:34 +0600

    add card

--- js/categories.js
+++ js/categories.js
@@ -12,13 +12,21 @@ const displayCatagories = (data)=>{
      data.forEach(element => {
           console.log()
          const buttonCatagories = document.createElement('div');
-         buttonCatagories.innerHTML=`
-       
-                <button  class= "btn lg:p-6 lg:text-2xl lg:2  " ><img class="h-5 w-5 lg:h-10 lg:w-10 md:h-8 md:w-8" src=${element.category_icon} />${element.category}</button>
-          
-
+         buttonCatagories.innerHTML=
+         `
+                <button  class= "btn category-btn lg:p-6 lg:text-2xl lg:2  " ><img class="h-5 w-5 lg:h-10 lg:w-10 md:h-8 md:w-8" src=${element.category_icon} />${element.category}</button>
          `
          CategoriesContainer.append(buttonCatagories)
      });
 }
+//removeActiveClass
+const removeActiveClass=()=>{
+     const buttons = document.getElementsByClassName("category-btn");
+     
+     for(let btn of buttons){
+       btn.classList.remove('active');
+     }
+   }
+
+
 loadCatagories()
