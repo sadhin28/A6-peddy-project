cf097d59584cdd8b3cb03566a77d10d7b4174a5f
sadhin28 <taosifsadhin113@gmail.com>
2025-04-23 17:09:11 +0600

    add individual catagories

--- js/categories.js
+++ js/categories.js
@@ -13,11 +13,12 @@ const displayCatagories = (data)=>{
      
      const CategoriesContainer = document.getElementById('categoriesContainer');
      data.forEach(element => {
-            //  console.log(element.category.toLowerCase())
+             const ct =(element.id)
+             console.log(ct)
          const buttonCatagories = document.createElement('div');
          buttonCatagories.innerHTML=
          `
-                <button  onclick="individualCatagory(${element.category.toLowerCase()})"  class= "btn category-btn lg:p-6 lg:text-2xl lg:2  " ><img class="h-5 w-5 lg:h-10 lg:w-10 md:h-8 md:w-8" src=${element.category_icon} />${element.category}</button>
+                <button id="pets"  onclick="displayPetsIndividual(${ct})"  class= "btn category-btn lg:p-6 lg:text-2xl lg:2  " ><img class="h-5 w-5 lg:h-10 lg:w-10 md:h-8 md:w-8" src=${element.category_icon} />${element.category}</button>
          `
          CategoriesContainer.append(buttonCatagories)
       
@@ -30,11 +31,22 @@ const displayCatagories = (data)=>{
 
 
 //showIndividualCatagoriPet
-const individualCatagory =(idd)=>{
-    
-    fetch(`https://openapi.programming-hero.com/api/peddy/category/${idd}`)
-    .then((res)=>res.json())
-    .then((data)=>displayPet(data.category))
+const individualCatagory =(id)=>{
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+        .then((res)=>res.json())
+        .then((data)=>displayPet(data.data))
+        const PetContainer = document.getElementById('petContainer');
+        PetContainer.innerText=""
 }
 
+const displayPetsIndividual=(id)=>{
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then(res => res.json())
+    .then((data)=> {
+        individualCatagory(data.categories[id-1].category.toLowerCase())
+    })
+    .catch((error) => console.log(error) )
+   
+}
 loadCatagories()
+
